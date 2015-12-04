92ab6c8fc9eb09fbbca2fd0c71faab477f95453f
brianyang <brian.yang@qunar.com>
2015-12-04 12:24:19 +0800

    add redis console

--- web_main.py
+++ web_main.py
@@ -7,7 +7,9 @@ from service import Session
 from flask_admin import Admin
 from modelview import ArticleView, TagView
 from flask_admin.contrib.sqla import ModelView
+from flask_admin.contrib import rediscli
 from model import Category
+import redis
 import sys
 
 reload(sys)
@@ -22,6 +24,9 @@ admin = Admin(app, name='flask_admin', template_mode='bootstrap3')
 admin.add_view(ArticleView(session))
 admin.add_view(TagView(session))
 admin.add_view(ModelView(Category, session))
+pool = redis.ConnectionPool(host='localhost', port=6379, db=0, password='Djhd1234')
+r = redis.StrictRedis(connection_pool=pool)
+admin.add_view(rediscli.RedisCli(r))
 
 app.secret_key = '\xbd$\x96\xb4\x80GYt"\x01\x9bk+"\x0c\xbd+\xc2\xf7A\xcb\xea\xee\x89/\xbe)4\xce-\xa3qbrian'
-#app.run(host='0.0.0.0', port=1998, debug=True)
+app.run(host='0.0.0.0', port=1998, debug=True)
