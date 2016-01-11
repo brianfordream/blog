86941e997caf2f5e68f319e02ba7d14db9d1a20c
brianyang <brian.yang@qunar.com>
2016-01-11 20:06:32 +0800

    auto commit

--- web_main.py
+++ web_main.py
@@ -34,10 +34,7 @@ app.secret_key = '\xbd$\x96\xb4\x80GYt"\x01\x9bk+"\x0c\xbd+\xc2\xf7A\xcb\xea\xee
 
 @app.errorhandler(404)
 def page_not_found(error):
-    try:
-        return redirect('/')
-    except Exception, e:
-        logging.error(e)
+    return redirect('/')
 
 
 #app.run(host='0.0.0.0', port=1998, debug=True)
